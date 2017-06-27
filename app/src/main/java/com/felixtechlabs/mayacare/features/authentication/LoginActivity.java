d173557cc24bdd8b30bcf986af6875380634e2ec
Rohan <rohan@felixtechlabs.com>
2017-06-27 12:51:14 +0530

    Added login functionality

---
+++ app/src/main/java/com/felixtechlabs/mayacare/features/authentication/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.felixtechlabs.mayacare.features.authentication;
+
+import android.app.Activity;
+
+import com.felixtechlabs.mayacare.features.base.MCBaseActivity;
+
+/**
+ * Created by rohan on 27/6/17.
+ */
+
+public class LoginActivity extends MCBaseActivity{
+
+    @Override
+    protected Activity getActivity() {
+        return null;
+    }
+
+    @Override
+    protected int getLayout() {
+        return 0;
+    }
+
+    @Override
+    public boolean isToolbarPresent() {
+        return false;
+    }
+}
